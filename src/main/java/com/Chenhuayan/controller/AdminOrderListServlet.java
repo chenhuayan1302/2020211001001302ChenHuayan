3d7750f6bc763a6a90c9deb170d5727adb8f9ef5
chen huayan <2747734572@qq.com>
2022-05-31 23:20:26 +0800

    week15 commit

---
+++ src/main/java/com/Chenhuayan/controller/AdminOrderListServlet.java
@@ -0,0 +1,42 @@
+package com.Chenhuayan.controller;
+import com.Chenhuayan.dao.OrderDao;
+import com.Chenhuayan.model.Order;
+import com.Chenhuayan.model.Payment;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.sql.Connection;
+import java.util.List;
+
+@WebServlet("/admin/orderList")
+class AdminOrderListServlet extends HttpServlet {
+    private Connection con = null;
+
+    @Override
+    public void destroy() {
+        super.destroy();
+    }
+
+    @Override
+    public void init() throws ServletException {
+        con = (Connection) getServletContext().getAttribute("con");
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        List<Payment> paymentTypeList = Payment.findAllPayment(con);
+        request.setAttribute("paymentTypeList",paymentTypeList);
+        OrderDao orderDao = new OrderDao();
+        List<Order> orderList = orderDao.findAll(con);
+        request.setAttribute("orderList",orderList);
+        String path = "/WEB-INF/views/admin/orderList.jsp";
+        request.getRequestDispatcher(path).forward(request,response);
+    }
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        this.doGet(request, response);
+    }
+}
